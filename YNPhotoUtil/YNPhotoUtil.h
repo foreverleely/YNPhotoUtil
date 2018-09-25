c2765e03ccc4fa46b2262da11ae9f55740bb675f
liyangly <foreverleely@hotmail.com>
2018-09-25 16:37:22 +0800

    first commit

---
+++ YNPhotoUtil/YNPhotoUtil.h
@@ -0,0 +1,46 @@
+//
+//  YNPhotoUtil.h
+//  
+//
+//  Created by liyangly on 2018/9/10.
+//  Copyright © 2018年 makeupopular.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Photos/Photos.h>
+#import <UIKit/UIKit.h>
+
+@interface YNPhotoUtil : NSObject
+
+/**
+ 获取指定相册中照片
+ 
+ @param assetCollection 相册
+ @param ascending 升序OR降序
+ @return 排序后的集合
+ */
++ (NSArray<PHAsset *> *)getAllAssetWithAssetCollection:(PHAssetCollection *)assetCollection
+                                             ascending:(BOOL)ascending;
+
+/**
+ 获取asset对应的图片
+ 
+ @param asset asset
+ @param size 图片size
+ @param completion 图片
+ */
++ (void)getImageWithAsset:(PHAsset *)asset
+                     size:(CGSize)size
+               completion:(void (^)(UIImage *image))completion;
+
+
+/**
+ 获取asset对应图片
+ 
+ @param asset asset
+ @param completion 图片
+ */
++ (void)getImageWithAsset:(PHAsset *)asset
+               completion:(void (^)(UIImage *image))completion;
+
+@end
---
+++ YNPhotoUtil/YNPhotoUtil.m
@@ -0,0 +1,52 @@
+//
+//  YNPhotoUtil.m
+//
+//
+//  Created by liyangly on 2018/9/10.
+//  Copyright © 2018年 makeupopular.com. All rights reserved.
+//
+
+#import "YNPhotoUtil.h"
+
+@implementation YNPhotoUtil
+
+// 获取指定相册中照片
++ (NSArray<PHAsset *> *)getAllAssetWithAssetCollection:(PHAssetCollection *)assetCollection ascending:(BOOL)ascending
+{
+    // ascending:按照片创建时间排序 >> YES:升序 NO:降序
+    NSMutableArray<PHAsset *> *assets = [NSMutableArray array];
+    PHFetchOptions *option = [[PHFetchOptions alloc] init];
+    option.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:ascending]];
+    PHFetchResult *result = [PHAsset fetchAssetsInAssetCollection:assetCollection options:option];
+    [result enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (((PHAsset *)obj).mediaType == PHAssetMediaTypeImage) {
+            [assets addObject:obj];
+        }
+    }];
+    return assets;
+}
+
+// 获取asset对应的图片
++ (void)getImageWithAsset:(PHAsset *)asset size:(CGSize)size completion:(void (^)(UIImage *image))completion
+{
+    PHImageRequestOptions *option = [[PHImageRequestOptions alloc] init];
+    option.deliveryMode = PHImageRequestOptionsDeliveryModeOpportunistic;
+    option.networkAccessAllowed = YES;
+    [[PHCachingImageManager defaultManager] requestImageForAsset:asset targetSize:size contentMode:PHImageContentModeDefault options:option resultHandler:^(UIImage * _Nullable image, NSDictionary * _Nullable info) {
+        if (completion)  completion(image);
+    }];
+}
+
+// 获取asset对应的图片
++ (void)getImageWithAsset:(PHAsset *)asset completion:(void (^)(UIImage *image))completion
+{
+    PHImageRequestOptions *option = [[PHImageRequestOptions alloc] init];
+    option.deliveryMode = PHImageRequestOptionsDeliveryModeOpportunistic;
+    option.networkAccessAllowed = YES;
+    [[PHCachingImageManager defaultManager] requestImageDataForAsset:asset options:option resultHandler:^(NSData * _Nullable imageData, NSString * _Nullable dataUTI, UIImageOrientation orientation, NSDictionary * _Nullable info) {
+        UIImage *image = [UIImage imageWithData:imageData];
+        if (completion) completion(image);
+    }];
+}
+
+@end
